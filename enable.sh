c4e4f241dcde4321b87954a01f2f80b9e4b84282
Xiao <xiao@scp-079.org>
2019-10-17 03:15:46 +0000

    - add enable

--- enable.sh
+++ enable.sh
@@ -2,6 +2,6 @@
 
 for bot in $(ls ~/scp-079); do
 	if [ "$bot" != "conda" ] && [ "$bot" != "scripts" ] && [ "$bot" != "venv" ]; then
-		systemctl --user restart $bot
+		systemctl --user enable $bot
 	fi
 done
