ffbf94500560facb95f3b901add45802c3b31ff2
Xiao <xiao@scp-079.org>
2020-04-27 02:12:47 +0000

    修改 clear.sh

--- clear.sh
+++ clear.sh
@@ -2,7 +2,6 @@
 
 for bot in $(ls ~/scp-079); do
     if [ "$bot" != "scripts" ] && [ "$bot" != "venv" ] && ! [[ "$bot" =~ ^(conda)$ ]]; then
-        echo -e "\n\033[0;32mClearing the log file of ${bot^^}...\033[0m\n"
         cp /dev/null ~/scp-079/$bot/log
         echo -e "\n\033[0;32mBot ${bot^^}'s log file Cleared!\033[0m\n"
     fi
