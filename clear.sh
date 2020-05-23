558120b1b8e57a4b1a4e1b28a5f14f73a988e64d
Xiao <xiao@scp-079.org>
2020-05-23 03:23:00 +0000

    更新脚本

--- clear.sh
+++ clear.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ $# -eq 1 ];then
+    bot=$1
+    cp /dev/null ~/scp-079/$bot/log
+    echo -e "\n\033[0;32mBot ${bot^^}'s log file Cleared!\033[0m\n"
+    exit
+
 for bot in $(ls ~/scp-079); do
     if [ "$bot" != "scripts" ] && [ "$bot" != "venv" ] && ! [[ "$bot" =~ ^(conda)$ ]]; then
         cp /dev/null ~/scp-079/$bot/log
--- disable.sh
+++ disable.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [ $# -eq 1 ];then
+    bot=$1
+    echo -e "\n\033[0;32mDisabling the bot ${bot^^}...\033[0m\n"
+    systemctl --user stop $bot
+    systemctl --user disable $bot
+    echo -e "\n\033[0;32mBot ${bot^^} Disabled!\033[0m\n"
+    exit
+
 for bot in $(ls ~/scp-079); do
     if [ "$bot" != "scripts" ] && [ "$bot" != "venv" ] && ! [[ "$bot" =~ ^(conda)$ ]]; then
         echo -e "\n\033[0;32mDisabling the bot ${bot^^}...\033[0m\n"
