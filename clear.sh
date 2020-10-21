903a78928938adccb87625cbf27ea7373612f89e
Xiao <xiao@scp-079.org>
2020-10-21 16:32:50 +0800

    update log.sh

--- clear.sh
+++ clear.sh
@@ -3,11 +3,20 @@
 NOCOLOR="\033[0m"
 GREEN="\033[0;32m"
 
-# TODO 适应新的 data 文件夹结构
 if [ $# -eq 1 ];then
     bot=$1
-    cp /dev/null ~/scp-079/"$bot"/log
-    echo -e "\n${GREEN}Bot ${bot^^}'s log file Cleared!${NOCOLOR}\n"
+    cd ~/scp-079/"$bot" || exit
+
+    # TODO TEMP
+    if [ -f "examples/config.ini" ]; then
+        LOG_PATH="log/log"
+    else
+        LOG_PATH="log"
+    fi
+
+    cp /dev/null "$LOG_PATH"
+    rm -f ~/scp-079/"$bot"/data/log/log-*
+    echo -e "\n${GREEN}Bot ${bot^^}'s log files Cleared!${NOCOLOR}\n"
     exit
 fi
 
@@ -15,8 +24,20 @@ shopt -s nullglob
 for bot in ~/scp-079/*; do
     bot=$(basename "$bot")
     if [ "$bot" != "scripts" ] && [ "$bot" != "venv" ] && ! [[ "$bot" =~ ^(conda)$ ]]; then
-        cp /dev/null ~/scp-079/"$bot"/log
-        echo -e "\n${GREEN}Bot ${bot^^}'s log file Cleared!${NOCOLOR}\n"
+        cd ~/scp-079/"$bot" || exit
+
+        # TODO TEMP
+        if [ -f "examples/config.ini" ]; then
+            LOG_PATH="log/log"
+        else
+            LOG_PATH="log"
+        fi
+
+        cp /dev/null "$LOG_PATH"
+        rm -f ~/scp-079/"$bot"/data/log/log-*
+        echo -e "\n${GREEN}Bot ${bot^^}'s log files Cleared!${NOCOLOR}\n"
     fi
 done
 shopt -u nullglob
+
+cd ~ || exit
